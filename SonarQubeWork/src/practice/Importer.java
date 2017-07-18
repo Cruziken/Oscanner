5b9ec016535dfbfd5069628ed54a5afa884451b4
Cruziken <bencruz@vols.utk.edu>
2017-07-18 10:39:51 -0400

    Trying to get the property writer configured

--- SonarQubeWork/src/practice/Importer.java
+++ SonarQubeWork/src/practice/Importer.java
@@ -1,5 +1,8 @@
 package practice;
 
 public class Importer {
-	
+	public void main(String[] args){
+		PropsWriter myPropsWriter = new PropsWriter("sonar.properties");
+		myPropsWriter.propsVars("sonarqubework");
+	}
 }
--- SonarQubeWork/src/practice/PropsWriter.java
+++ SonarQubeWork/src/practice/PropsWriter.java
@@ -2,52 +2,80 @@ package practice;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 
-
-
 public class PropsWriter {
-	//Creates a FileWriter variable named fileWriter
-	private FileWriter fileWriter;
+	// Creates a FileWriter variable named fileWriter
+	private FileOutputStream fileOut;
 	// Creates a BufferedWriter variable named buffWriter
-	private BufferedWriter buffWriter;
+	private File file;
 	private Properties configProperty;
-	public PropsWriter() {
-		 configProperty = new Properties();
+
+	public PropsWriter(String filename) {
+		configProperty = new Properties();
+		file = new File(filename);
+		try {
+			fileOut = new FileOutputStream(file, true);
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Check your constructer");;
+		}
 	}
-	public void propsVars(String projectname){
+
+	public void propsVars(String projectname) {
 		String keyDescript = "# The project key";
-		String projKey = "sonar.projectKey=my:" ;
+		String projKey = "sonar.projectKey=my:";
 		String otherDescript = "#name, version, source folder";
 		String projName = "sonar.projectName=My ";
 		String projValue = projectname;
 		String versionKey = "sonar.projectVersion=";
 		String versionVal = "1.0";
-		String sourceKey= "sonar.sources=";
+		String sourceKey = "sonar.sources=";
 		String sourceVal = "./src";
+		propsWriter(keyDescript);
 		propsWriter(projKey, projValue);
+		propsWriter(otherDescript);
 		propsWriter(projName, projValue);
 		propsWriter(versionKey, versionVal);
 		propsWriter(sourceKey, sourceVal);
 	}
-	public void propsWriter(String key, String data) {
+
+	public void propsWriter(String content) {
+		byte[] bytesArray = content.getBytes();
+		try {
+			fileOut.write(bytesArray);
+			fileOut.flush();
+			fileOut.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Check your propsWriter(String content) method");
+		}
 		
+	}
+
+	public void propsWriter(String key, String data) {
+
 		configProperty.setProperty(key, data);
-		File file = new File("D:\\Helper.properties");
-		FileOutputStream fileOut = new FileOutputStream(file,true);
-		configProperty.store(fileOut, "sample properties");
-		fileOut.close();
+
+		try {
+			
+			configProperty.store(fileOut, "sample properties");
+			fileOut.close();
+		} catch (IOException e) {
+			System.out.println("File was a dud in PropsWriter");
 
 			/**
-			 * Method allows the passing of filenames from other classes. Creates a new
-			 * File and also creates new instances of fileWriter and buffWriter
+			 * Method allows the passing of filenames from other classes.
+			 * Creates a new File and also creates new instances of fileWriter
+			 * and buffWriter
 			 * 
 			 * @param filename
-			 * @return 
+			 * @return
 			 */
+		}
 	}
 }
-	
