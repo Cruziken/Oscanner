9ad83cef8f17dbfa12c49464c46b9b1292db9476
Ben Cruz <bencruz@vols.utk.edu>
2017-07-18 14:55:23 -0400

    Add files via upload

---
+++ SonarQubeWork/src/practice/AbsolutePather.java
@@ -0,0 +1,45 @@
+package practice;
+
+import java.io.File;
+
+public class AbsolutePather {
+
+		/**
+		 * This method takes a String filename and returns the absolute path of the
+		 * file passed to it
+		 * 
+		 * @param filename
+		 * @return
+		 */
+		public String getAbsPath(String filename) {
+
+			// Creates a string called path that'll house our absolute file name
+			String path = "";
+			// Creates a boolean bool which is true
+			boolean bool;
+			// In case there is no filename string to turn into a file...
+			try {
+				// create new file out of passed filename String
+				File file = new File(filename);
+
+				// returns true if the file exists
+				bool = file.exists();
+
+				// if file exists
+				if (bool) {
+					// get absolute path
+					path = file.getAbsolutePath();
+				}
+
+			}
+			// Catch the exception
+			catch (Exception e) {
+
+				// if any error occurs
+				System.out.println("Absolute path went rouge");
+			}
+			// Return the absolute path of the filename
+			return path;
+		}
+	}
+
