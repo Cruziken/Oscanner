ef1438662f365dbc8c3bc62bd3e90d981b7e7f8e
Cruziken <bencruz@vols.utk.edu>
2017-07-18 09:00:58 -0400

    Added another println test

--- SonarQubeWork/src/practice/PropsWriter.java
+++ SonarQubeWork/src/practice/PropsWriter.java
@@ -3,5 +3,6 @@ package practice;
 public class PropsWriter {
 	public static void main(String[] args){
 	System.out.println("Practicing stuff.");
+	System.out.println("Making sure I remember");
 	}
 }
