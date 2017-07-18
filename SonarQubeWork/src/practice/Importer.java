1eff5a0916005652f1950f74d963540085b88af7
Ben Cruz <bencruz@vols.utk.edu>
2017-07-18 16:00:03 -0400

    Update Importer.java

--- SonarQubeWork/src/practice/Importer.java
+++ SonarQubeWork/src/practice/Importer.java
@@ -1,12 +1,49 @@
 package practice;
 
 public class Importer {
-	public static void main(String[] args) {
-		PropsWriter myPropsWriter = new PropsWriter("sonar-project.properties");
-		Variables myVariables = new Variables();
-		// PullWithGit myPWG = new PullWithGit();
-		myVariables.vars("sonarqubework");
-		myPropsWriter.propsVars(myVariables);
+	
+	public void importIt(){
+		String filena= "FizzBuzz";
+		AbsolutePather myAbsPather = new AbsolutePather();
+		if (filena!=null){
+		String abspath= myAbsPather.getAbsPath(filena);
+		  String[] command = {"CMD", "/C", "dir"};
+	        ProcessBuilder probuilder = new ProcessBuilder( command );
+	        //You can set up your work directory
+	        probuilder.directory(new File(abspath));
+	        
+	        Process process = null;
+			try {
+				process = probuilder.start();
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+	        
+	        //Read out dir output
+	        InputStream is = process.getInputStream();
+	        InputStreamReader isr = new InputStreamReader(is);
+	        BufferedReader br = new BufferedReader(isr);
+	        String line;
+	        System.out.printf("Output of running %s is:\n", Arrays.toString(command));
+	        try {
+				while ((line = br.readLine()) != null) {
+				    System.out.println(line);
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        
+	        //Wait to get exit value
+	        try {
+	            int exitValue = process.waitFor();
+	            System.out.println("\n\nExit Value is " + exitValue);
+	        } catch (InterruptedException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	    }
+	
 	}
 }
-
