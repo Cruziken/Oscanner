9c12ee4dddbe48cf9db514e106ceec99d63a7f7e
Ben Cruz <bencruz@vols.utk.edu>
2017-07-19 12:30:23 -0400

    Update Importer.java

--- SonarQubeWork/src/practice/Importer.java
+++ SonarQubeWork/src/practice/Importer.java
@@ -1,49 +1,71 @@
 package practice;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Importer {
-	
-	public void importIt(){
-		String filena= "FizzBuzz";
-		AbsolutePather myAbsPather = new AbsolutePather();
-		if (filena!=null){
-		String abspath= myAbsPather.getAbsPath(filena);
-		  String[] command = {"CMD", "/C", "sonar-scanner"};
-	        ProcessBuilder probuilder = new ProcessBuilder( command );
-	        //You can set up your work directory
-	        //probuilder.directory(new File(abspath));
-	        
-	        Process process = null;
-			try {
-				process = probuilder.start();
-			} catch (IOException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
+	//public void impToSq() {
+	public static void main (String[] args){
+		Variables myVari = new Variables();
+		String commands = myVari.getStartCommand();
+		String directory = myVari.getStartDirectory();
+		//myVari.getSendCommand();
+		//myVari.getFileSendDirectory();
+		// public void importIt(){
+		// String filena= "CruzCaliber.txt";
+		// AbsolutePather myAbsPather = new AbsolutePather();
+		// if (filena!=null){
+		// String abspath= myAbsPather.getAbsPath(filena);
+		// System.out.println(abspath);
+		//String dir = myAbsPather.getAbsPath(filena);
+		 //Directory if you want to analyze a project must be C://...project
+		 String dir = directory;
+		System.out.println(dir);
+		String[] command = { "CMD", "/C", commands };
+		ProcessBuilder probuilder = new ProcessBuilder(command);
+		probuilder.redirectErrorStream(true);
+		// You can set up your work directory
+		// How to get path from working directory
+		probuilder.directory(new File(dir));
+		Process process = null;
+		try {
+			process = probuilder.start();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		// Read out dir output
+		InputStream is = process.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		System.out.printf("Output of running %s is:\n", Arrays.toString(command));
+		try {
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
 			}
-	        
-	        //Read out dir output
-	        InputStream is = process.getInputStream();
-	        InputStreamReader isr = new InputStreamReader(is);
-	        BufferedReader br = new BufferedReader(isr);
-	        String line;
-	        System.out.printf("Output of running %s is:\n", Arrays.toString(command));
-	        try {
-				while ((line = br.readLine()) != null) {
-				    System.out.println(line);
-				}
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-	        
-	        //Wait to get exit value
-	        try {
-	            int exitValue = process.waitFor();
-	            System.out.println("\n\nExit Value is " + exitValue);
-	        } catch (InterruptedException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        }
-	    }
-	
-	}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// Wait to get exit value
+		try {
+			int exitValue = process.waitFor();
+			System.out.println("\n\nExit Value is " + exitValue);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// }
+		/*
+		 * try { PrintWriter out = new PrintWriter("StartSonar.bat"); } catch
+		 * (IOException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace();
+		 * */
+		 }
 }
