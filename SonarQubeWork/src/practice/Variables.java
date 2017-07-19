2017d686a22759952fcc5cf294ecac9cddbb3f01
Cruziken <bencruz@vols.utk.edu>
2017-07-19 12:56:04 -0400

    Var Changes

--- SonarQubeWork/src/practice/Variables.java
+++ SonarQubeWork/src/practice/Variables.java
@@ -13,7 +13,7 @@ public class Variables {
 	private String sendCommand;
 	
 	public void sonarVars(){
-		 startDirectory = "C:\\Users\\fz3\\SonarQube\\sonarqube-6.4\\sonarqube-6.4";
+		 startDirectory = "C:\\Users\\fz3\\SonarQube\\sonarqube-6.4\\sonarqube-6.4\\bin\\windows-x86-64";
 		 fileSendDirectory = "C:\\Users\\fz3\\FizzBuzz";
 		 startCommand = "StartSonar.bat";
 		 sendCommand = "sonar-scanner";
