4954029cd1e75bea11b1053365a636a51b3193e3
Cruziken <bencruz@vols.utk.edu>
2017-07-20 10:46:32 -0400

    Importer class organized. Untested

--- SonarQubeWork/src/practice/Importer.java
+++ SonarQubeWork/src/practice/Importer.java
@@ -6,68 +6,99 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+/*
+ * This class runs command prompt arguments
+ */
 
 public class Importer {
-	//public void impToSq() {
-	public static void main (String[] args){
+	private Process process;
+	private String commands, directory;
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * This method sets the Variables
+	 */
+	public void setVars() {
+		// Creates an instance of Variables named myVari
 		Variables myVari = new Variables();
+		// Calls the sonarVars to get acces to its methods
 		myVari.sonarVars();
-		//myVari.fileVars("project");
-		String commands = myVari.getStartCommand();
-		String directory = myVari.getStartDirectory();
-		//String commands = myVari.getSendCommand();
-		//String directory = myVari.getFileSendDirectory();
+		// gets the commands it needs from Variables
+		commands = myVari.getStartCommand();
+		// gets the directory to start commands from
+		directory = myVari.getStartDirectory();
+		// gets the commands it needs from Variables
+		// String commands = myVari.getSendCommand();
+		// gets the directory to start commands from
+		// String directory = myVari.getFileSendDirectory();
+		// Prints out the commands
 		System.out.println(commands);
-		// public void importIt(){
-		// String filena= "CruzCaliber.txt";
-		// AbsolutePather myAbsPather = new AbsolutePather();
-		// if (filena!=null){
-		// String abspath= myAbsPather.getAbsPath(filena);
-		// System.out.println(abspath);
-		//String dir = myAbsPather.getAbsPath(filena);
-		 //Directory if you want to analyze a project must be C://...project
+		// Prints out the directory
 		System.out.println(directory);
+	}
+
+	/**
+	 * This method runs commands through command prompt
+	 */
+	public void commandPrompt() {
+		// Creates this OS specific command. In this case it is windows
 		String[] command = { "CMD", "/C", commands };
+		// Creates an instance of ProcessBuilder called probuilder given the
+		// command
 		ProcessBuilder probuilder = new ProcessBuilder(command);
+		// Redirects errosStream to eclipse console
 		probuilder.redirectErrorStream(true);
 		// You can set up your work directory
 		// How to get path from working directory
 		probuilder.directory(new File(directory));
-		Process process = null;
+		// In case process is a dud
 		try {
+			// Starts the process
 			process = probuilder.start();
-		} catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
+			// Sends command to sendToConsolemethod
+			sendToConsole(command);
 		}
-		// Read out dir output
-		InputStream is = process.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		String line;
-		System.out.printf("Output of running %s is:\n", Arrays.toString(command));
-		try {
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
-			}
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		// ..print off the following statement
+		catch (IOException e2) {
+			// Inform that error ocurred here
+			System.out.println("Something happened in ");
 		}
+	}
 
-		// Wait to get exit value
+	/**
+	 * Method gets commandPrompt statements to output to Eclipse console
+	 * 
+	 * @param command
+	 */
+	public void sendToConsole(String[] command) {
+		// Creates an instance of InputStream to get content from process
+		InputStream is = process.getInputStream();
+		// Creates an inputStreamreader to read content from inputstream
+		InputStreamReader isr = new InputStreamReader(is);
+		// Reads characterinput Stream from InputStreamReader
+		BufferedReader br = new BufferedReader(isr);
+		// Creates a String variable called line
+		String line;
+		// Converts whatever command to console
+		System.out.printf("Output of running %s is:\n", Arrays.toString(command));
+		// In case the lines are duds
 		try {
-			int exitValue = process.waitFor();
-			System.out.println("\n\nExit Value is " + exitValue);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// While the bufferedreader is giving content and no null values do
+			// the following
+			while ((line = br.readLine()) != null) {
+				// Prints off the content the BufferedReader gives
+				System.out.println(line);
+				int exitValue = process.waitFor();
+				System.out.println("\n\nExit Value is " + exitValue);
+			}
 		}
-		// }
-		/*
-		 * try { PrintWriter out = new PrintWriter("StartSonar.bat"); } catch
-		 * (IOException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace();
-		 * */
-		 }
+		// ...print the followng statement if something goes wrong
+		catch (IOException | InterruptedException e1) {
+			// Inform that a mistake happened in sendToConsole
+			System.out.println("Something went wrong in sendToConsole");
+		}
+	}
 }
