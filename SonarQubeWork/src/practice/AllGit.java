5c08fcd8faf9ea96bd31311f7fd6bb6fbfc0440a
Cruziken <bencruz@vols.utk.edu>
2017-07-20 09:31:19 -0400

    Partially commented AllGit

--- SonarQubeWork/src/practice/AllGit.java
+++ SonarQubeWork/src/practice/AllGit.java
@@ -1,60 +1,107 @@
 package practice;
 //import java.io.File;
-//import java.io.IOException;
 
-//import org.eclipse.jgit.api.CloneCommand;
-//import org.eclipse.jgit.api.Git;
-//import org.eclipse.jgit.api.errors.GitAPIException;
-//import org.eclipse.jgit.internal.storage.file.FileRepository;
-//import org.eclipse.jgit.lib.Repository;
 
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.CreateBranchCommand.SetupUpstreamMode;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
 
 public class AllGit {
-	public void pull(){
-		/*private String localPath, remotePath;
+	// Creates a localPath String (where and what the local when will be called)
+	// and a remotePath String(which repository I'm getting it from
+	private String localPath, remotePath;
+	// Creates an instance of Repository called localRepo
 	private Repository localRepo;
+	// Creates an instance of Git called git
 	private Git git;
 
-	public void init() {
+	/**
+	 * This method Initializes the localPath and remotePath variables, the
+	 * localRepo, and the Git instance
+	 */
+	public void initialize() {
+		// Assigns the localPath a value. May have to passed in from main class
+		// when SonarQube work is made into a Maven project
 		localPath = "C:\\Users\\fz3\\SonarQube\\TheClone";
+		// Assigns the remotePath a value. May have to passed in from main class
+		// when SonarQube work is made into a Maven project
 		remotePath = "https://github.com/Cruziken/SonarQube.git";
+		// In case localPath is null...
 		try {
+			// Assigns the localRepo a value defined by localPath
 			localRepo = new FileRepository(localPath + "/.git");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+		// Print off the following statement
+		catch (IOException e) {
+			// Let us know that error occured here
+			System.out.println("Something happened in method init");
+		}
+		// Assign git the value of the new repo
 		git = new Git(localRepo);
 	}
 
-	public void testCreate() {
+	/**
+	 * This method creates a new local repository
+	 */
+	public void createLocRepo() {
+		// The new local Repository that will be created
 		Repository newRepo;
+		// In case the local path is null...
 		try {
+			// Assigns the new repo the value from the localPath
 			newRepo = new FileRepository(localPath + ".git");
+			// Creates this repository
 			newRepo.create();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		// Print the following statement
+		catch (IOException e) {
+			// Let us know that error occured here
+			System.out.println("Something happened in testCreate");
 		}
 	}
 
-	public void testClone() {
+	/**
+	 * This method clones a remote repository to the local one on the machine
+	 */
+	public void createClone() {
+		// In case the paths are null...
 		try {
+			// Sets the directory from the localPath, sets the remotePath,
+			// clones the values from the remote repository to the local
+			// repository
 			Git.cloneRepository().setURI(remotePath).setDirectory(new File(localPath)).call();
-		} catch (GitAPIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		// ...Print out the following statement
+		catch (GitAPIException e) {
+			// Let someone know error occured here
+			System.out.println("Something happened in testClone");
 		}
 	}
 
-	public void testAdd() {
-		File myfile = new File(localPath + "C:\\Users\\fz3\\workspace\\FizzBuzz\\CruzCaliber.txt");
+	/**
+	 * This method adds files to the local repository
+	 */
+	public void addFile() {
+		// Assigns the instance of File the localPath plus file you want to
+		// create
+		File myfile = new File(localPath + "CruzCaliber.txt");
+		// In case myfile is a dud...
 		try {
+			// Create the new file
 			myfile.createNewFile();
+			// Adds the new file to the localrepo
 			git.add().addFilepattern("myfile").call();
-		} catch (GitAPIException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		// Print off the following statement
+		catch (GitAPIException | IOException e) {
+			// Inform something went wrong in this method
+			System.out.println("Something went wrong in testAdd");
 		}
 	}
 
@@ -88,7 +135,5 @@ public class AllGit {
 
 	public void testPull() throws IOException, GitAPIException {
 		git.pull().call();
-	}
-}		} */
 	}
 }
