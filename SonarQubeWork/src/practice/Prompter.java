cfa9e1a5430feaf54c68e7aa4506ced65349ea61
Cruziken <bencruz@vols.utk.edu>
2017-07-20 16:33:55 -0400

    Added comments to Prompter

--- SonarQubeWork/src/practice/Prompter.java
+++ SonarQubeWork/src/practice/Prompter.java
@@ -2,24 +2,43 @@ package practice;
 
 import java.util.Scanner;
 
+/**
+ * This class returns input that it requests
+ * 
+ * @author fz3
+ *
+ */
 public class Prompter {
+	// This creates a variable called values
 	private String values;
-	private Scanner input;
+	// This creates a Scanner called input
+	private Scanner scanner;
 
+	/**
+	 * This method accepts prompts and returns the user input
+	 * 
+	 * @param value
+	 * @return
+	 */
 	public String scanIt(String value) {
+		// Print out the prompt for the user
 		System.out.println("Please enter the value for " + value + ".");
 		// Creates an instance of Scanner named input.
-		input = new Scanner(System.in);
+		// Assigns the scanner whatever value is inputed
+		scanner = new Scanner(System.in);
 		{
-			// Creates a string out of Text found within file.
-			values = input.nextLine();
-			// Prints num string from past line
-
+			// Creates a string called values that capture sthe input
+			values = scanner.nextLine();
 		}
+		// Returns the input scanned
 		return values;
 	}
 
+	/**
+	 * Method that closes the scanner
+	 */
 	public void closeIt() {
-		input.close();
+		// closes the scanner
+		scanner.close();
 	}
 }
