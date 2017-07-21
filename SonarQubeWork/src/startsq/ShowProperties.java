94a7f056afcc3510db752ca2cf34dd048df65743
Cruziken <bencruz@vols.utk.edu>
2017-07-21 09:07:47 -0400

    Messing with fixing os bug in commander.commandPrompt()

---
+++ SonarQubeWork/src/startsq/ShowProperties.java
@@ -0,0 +1,8 @@
+package startsq;
+
+public class ShowProperties {
+	    public static void main(String[] args) {
+	     String os =  System.getProperty("os.name");
+	       System.out.println(os);
+	    }
+	}
