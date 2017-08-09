7605efdeb2abb243cb08c5f1b6426b8d650923c4
Cruziken <bencruz@vols.utk.edu>
2017-08-09 13:00:23 -0400

    Local repo should be C:\\...\\Clone\\ or /home/.../Clone/

--- SonarQubeWork/src/sonarqube/Oscanner.java
+++ SonarQubeWork/src/sonarqube/Oscanner.java
@@ -10,8 +10,8 @@ import java.util.List;
  */
 public class Oscanner {
 	// This is the localpath where the remote Repository will be cloned
-	// to.Examples: C:\\Users\\fz3\\SonarQube\\TheClone on Windows and
-	// /home/mint/Programs/TheClone on linux
+	// to.Examples: C:\\Users\\fz3\\SonarQube\\TheClone\\ on Windows and
+	// /home/mint/Programs/TheClone/ on linux
 	private static String localRepo = null;
 	// This is the remoteRepo URL; what will be cloned. Example:
 	// https:://github.com/Cruziken/SonarQube-Practice.git
@@ -61,7 +61,7 @@ public class Oscanner {
 			// Calls the fileVars method on myVariables. Sets the contents of
 			// the
 			// properties file
-			myVariables.fileVars(sub, source);
+			myVariables.fileVars((sub), source);
 			// Gets the values from Varibles to print to the properties file
 			myPropsWriter.propsVars(myVariables);
 			// Gets the directory and command from myVariables class
