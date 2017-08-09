23b67b2e3fc4e1fe159e1eb85a3b4b77469844f1
Cruziken <bencruz@vols.utk.edu>
2017-08-09 10:45:18 -0400

    Fixed the bug. It successfully loops through the subdirectories

--- SonarQubeWork/src/sonarqube/DirectoryScanner.java
+++ SonarQubeWork/src/sonarqube/DirectoryScanner.java
@@ -5,117 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 	public class DirectoryScanner {
-		//private static File folder = new File("C:\\Users\\fz3\\workspace");
+		
 				private static String dirContent;
-				private static int numOfSubfolders;
 				private static String directPath;
-				private static List<String> subPaths;
-				 private static List<String> subPathFake;
-				//public static void listFilesForFolder(final File folder) {
-				  /*  for (final File fileEntry : folder.listFiles()) {
-				        if (fileEntry.isDirectory()) {
-				            listFilesForFolder(fileEntry);
-				        } else {
-				            System.out.println(fileEntry.getName());
-				        }
-				    } */
-				//public static void main(String[] args){
-				//	listFolders();
-					
-				//}
+				 private static List<String> subPaths;
+				
 				public DirectoryScanner(String directPather){
 					 //directPath = "C:\\Users\\fz3\\workspace";
 					directPath=directPather;
-					 listFolders();
-				}
-				public void doSB(String content){
-					StringBuilder sb = new StringBuilder();
-					String dot = ".";
-					for (int i = 1; i < 18; i++){
-						sb.append(content);
-						sb.append(dot);
-						sb.append(i);
-					}
 				}
 				 public List<String> listFolders(){
 					 File directory = new File(directPath);
 				        //get all the files from a directory
 				        File[] fList = directory.listFiles();
-				        subPathFake = new ArrayList<String>();
+				        subPaths = new ArrayList<String>();
 				        for (File file : fList){
 				            if (file.isDirectory()){
 				            	 dirContent= file.getName();
-				            	// System.out.println(dirContent);
-				            	// while (dirContent!=null){
-				            	 //arrayIt(dirContent);
-				            	 subPathFake.add(dirContent);
-				            	// System.out.println(subPathFake.size());
-				          
-				            	// }
+				            	 subPaths.add(dirContent);
+				            	
 				            	}
-				          //  System.out.println(subPathFake.size());
-				            
+				        }
+				        System.out.println("Number of subdirectories = " + subPaths.size());
+						return subPaths;      
 				           
 				            
 				         
 				        }
-				        
-				     
-				               // dirArray[i]= dirContent;
-				                //System.out.println(dirContent);
-				        //System.out.println(dirContent);
-				       // return dirContent;
-						System.out.println("I am here");
-						//System.out.println(dirContent);
-						return subPathFake;}
-				 public <T> List<T> magicalListGetter() {
-					    return new ArrayList<T>();
-					}    
-				        
-				        //System.out.println(dirArray);
-		               // System.out.println(dirContent);
-				        //return dirContent;
-				    
-				public static void arrayIt(List<String> sub){
-					//String[] directC = new String[numDirect(directPath)];
-					//int number = numOfSubfolders;
-		            //for (int i = 0; i<1; i++){
-					//directC[i]= dirContent;
-					//int i =0;
-					//i++;
-					
-					//while (i<number){
-					subPaths= (sub);
-					
-		            //}
-				
-					 } 
-				 
-				 public static int numDirect(String directoryName) {
-				 File dir = new File(directoryName);
-				 numOfSubfolders = 0;
-				 File listDir[] = dir.listFiles();
-				 for (int i = 0; i < listDir.length; i++) {
-				     if (listDir[i].isDirectory()) {
-				             numOfSubfolders++;
-				         }
-				/* String[] dirArray= new String[numOfSubfolders];
-				 for (int j = 0; j < dirArray.length; i++){ 
-				 //listFolders(directoryName);
 				 }
-				 System.out.println("No of dir " + numOfSubfolders); 
-				 }
-				 }*/
-				 }
-				return numOfSubfolders;
-				 }
-				 public List<String> getSubPaths(){
-					 return subPaths;
-				 }
-				 }
-				    
-			
-				
-				
-				    
-				//listFilesForFolder(folder);}
--- SonarQubeWork/src/sonarqube/Oscanner.java
+++ SonarQubeWork/src/sonarqube/Oscanner.java
@@ -1,10 +1,10 @@
 package sonarqube;
 
-import java.io.File;
 import java.util.List;
 
 /**
- * This main method starts the program
+ * This class contains the main method that starts the program and the recipe
+ * of events that take place
  * 
  * @author fz3
  *
@@ -17,57 +17,62 @@ public class Oscanner {
 	// This is the remoteRepo URL; what will be cloned. Example:
 	// https:://github.com/Cruziken/SonarQube-Practice.git
 	private static String remoteRepo = null;
-	// This is projectName for the project. sonarTest
-	//private static String projectName = null;
 	// This is the name of the properties file. Should not change
 	private static final String filename = "sonar-project.properties";
 	// This is the name of the source in the project. For example . or ./src or
 	// ./SonarQubeWork/src
 	private static String source = null;
-	private static List<String> subPaths= null;
+	// Creates a List of Strings called subPaths. These are the subdirectories
+	// within the cloned github repository
+	private static List<String> subPaths = null;
 
-	           
-	
+	/**
+	 * This method does it all
+	 */
 	public static void startIt() {
 		// Creates an instance of AllGit and passes the localpath and the remote
-		// UR. to it
-		
+		// URL to it
 		AllGit myAllGit = new AllGit(localRepo, remoteRepo);
+		//// Creates a clone in localpath location of remote repository
 		myAllGit.createClone();
-		
+		// Creates an instance of DirectoryScanner. Passes the local repository
+		// path through the constructor
 		DirectoryScanner myDirScan = new DirectoryScanner(localRepo);
+		// This method creates a list of the subdirectories from the localRepo
+		// path passed through
 		subPaths = myDirScan.listFolders();
-			for (int i=0;i < subPaths.size();i++){
-		String sub = subPaths.get(i);
-		System.out.println(sub);
-		System.out.println(subPaths.size());
-		// Creates an instance of PropsWriter and passes the filename and
-		// localRepo variables through it
-		
-		// Creates a clone in localpath location of remote repository
-		
-		PropsWriter myPropsWriter = new PropsWriter(filename, (localRepo +sub));
-		// Creates an instance of Commander called myCommander
-		Commander myCommander = new Commander();
-		// Creates an instance of Variables called myVariables
-		Variables myVariables = new Variables();
-		
-		// Calls the sonarVars method on myVariables. This sets the directory
-		// that we will send from and tells where the sonar-scanner location
-		myVariables.sonarVars((localRepo + sub));
-		// Calls the fileVars method on myVariables. Sets the contents of the
-		// properties file
-		myVariables.fileVars(sub, source);
-		// Gets the values from Varibles to print to the properties file
-		myPropsWriter.propsVars(myVariables);
-		// Gets the directory and command from myVariables class
-		myCommander.setVars(myVariables);
-		// Calls the commandPrompt on myCommander. In this case, this sends the
-		// repo to the sonarqube scanner
-		myCommander.commandPrompt();}
+		// For look state takes the number of subdirectories in subPaths and
+		// says for each one starting from the first (subPaths.get(0)) to the
+		// last (subPaths.get<subPaths.size()), pass the subdirectory through
+		// the following methods
+		for (int i = 0; i < subPaths.size(); i++) {
+			// Takes the value for the subdirectory
+			String sub = subPaths.get(i);
+			// Creates an instance of PropsWriter and passes the filename and
+			// subdirectory variables through it
+			PropsWriter myPropsWriter = new PropsWriter(filename, (localRepo + sub));
+			// Creates an instance of Commander called myCommander
+			Commander myCommander = new Commander();
+			// Creates an instance of Variables called myVariables
+			Variables myVariables = new Variables();
+			// Calls the sonarVars method on myVariables. This sets the
+			// directory
+			// that we will send from and tells where the sonar-scanner location
+			myVariables.sonarVars((localRepo + sub));
+			// Calls the fileVars method on myVariables. Sets the contents of
+			// the
+			// properties file
+			myVariables.fileVars(sub, source);
+			// Gets the values from Varibles to print to the properties file
+			myPropsWriter.propsVars(myVariables);
+			// Gets the directory and command from myVariables class
+			myCommander.setVars(myVariables);
+			// Calls the commandPrompt on myCommander. In this case, this sends
+			// the
+			// repo to the sonarqube scanner
+			myCommander.commandPrompt();
+		}
 	}
-
-
 
 	/**
 	 * The main method is where the program looks to begin
@@ -75,13 +80,13 @@ public class Oscanner {
 	 * @param path
 	 */
 	public static void main(String[] path) {
-		// Calls the startIt() method
-		// myValues();
+		//Allows user input of localRepo value
 		localRepo = path[0];
+		//Allows user input of remoteRepo value
 		remoteRepo = path[1];
-		//projectName = path[2];
+		//Allows user input of source file. To be safe make it .
 		source = path[2];
+		// starts the whole shabang
 		startIt();
 	}
 }
-
